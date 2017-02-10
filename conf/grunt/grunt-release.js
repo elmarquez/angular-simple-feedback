a0808896e9fc409d4b17bac22fecaee2b04b5a09
Davis Marques <dmarques@colossus.lan>
2017-02-10 23:41:53 +1100

    improvement: Add bump task to release process

--- conf/grunt/grunt-release.js
+++ conf/grunt/grunt-release.js
@@ -1,7 +1,7 @@
 module.exports = function (grunt) {
   grunt.registerTask('release', 'Release a new application version',
     function () {
-      grunt.task.run(['compile','test','gitadd','gitcommit']);
+      grunt.task.run(['compile','test','gitadd','gitcommit','bump']);
     }
   );
 };
